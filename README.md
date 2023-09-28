8cc711d4a526a2aab694ab8f92f96708b50f93cf
ArZarLordOfMango <arzarchannel@gmail.com>
2023-09-28 22:38:39 +0200

    1.1.1

--- README.md
+++ README.md
@@ -4,18 +4,61 @@ Unix time like library for Rust
 
 Start point(SP) it's 00:00:00 01.01.2000.
 
-## Methods
+## Mtime Methods
 
-| name                 | return type | description                                        |
-|----------------------|-------------|----------------------------------------------------|
-| get_count_days()     | u64         | return number of days from SP.                     |
-| get_count_years()    | u64         | return number of year from SP.                     |
-| is_leap_year()       | bool        | return true if current year is leap else false. |
-| get_seconds()        | String      | return current seconds from 0 to 59.            |
-| get_minutes()        | String      | return current minutes from 0 to 59.            |
-| get_hours()          | String      | return current hours from 0 to 23.              |
-| get_day()            | String      | return current day from 1 to 28-31.             |
-| get_month()          | String      | return number of current month.                 |
-| get_month_name()     | String      | return name of current month.                   |
-| get_year()           | String      | return current year.                            |
-| get_full_date()      | String      | return full date (HH:MM:SS DD.MM.YYYY).         |
+| name                 | return type | description                                      |
+|----------------------|-------------|--------------------------------------------------|
+| get_count_days()     | u64         | returns number of days from SP.                  |
+| get_count_years()    | u64         | returns number of year from SP.                  |
+| is_leap_year()       | bool        | returns true if current year is leap else false. |
+| get_seconds()        | u8          | returns seconds.                                 |
+| get_visual_seconds() | String      | returns formatted seconds.                       |
+| get_minutes()        | u8          | returns minutes.                                 |
+| get_visual_minutes() | String      | returns formatted minutes.                       |
+| get_hours()          | u8          | returns hours.                                   |
+| get_visual_hours()   | String      | returns formatted hours.                         |
+| get_day()            | u8          | returns day.                                     |
+| get_visual_day()     | String      | returns the formatted day.                       |
+| get_month()          | u8          | returns the month number.                        |
+| get_visual_month()   | String      | returns the formatted month number.              |
+| get_month_name()     | &str        | returns the month name.                          |
+| get_year()           | u64         | returns the year.                                |
+| get_visual_year()    | String      | returns the formatted year.                      |
+| get_date()           | Date        | returns Date                                     |
+
+## Date Methods
+
+| name                 | return type | description                                      |
+|----------------------|-------------|--------------------------------------------------|
+| format(format)       | String      | returns formate Date.                            |
+
+### Date format
+
+| name format          | description |
+|----------------------|-------------|
+| %s                   | seconds     |
+| %m                   | minutes     |
+| %h                   | hours       |
+| %D                   | day         |
+| %M                   | month       |
+| %Y                   | year        |
+
+## Example
+
+```rust
+// Create a new instance of Mtime
+let example = mtime::Mtime::new(1234);
+```
+
+```rust
+// Get how many seconds from 0 to 59
+let example = mtime::Mtime::new(1234);
+println!("Seconds: {}", example.get_visual_seconds());
+```
+
+```rust
+// Exmaple work with Date
+let example = mtime::Mtime::new(1234); 
+let example_date = exmaple.get_date(); 
+println!("{}", exmaple_date.format("%h:%m:%s %D.%M.%Y")); 
+```
---
+++ src/lib.rs
@@ -0,0 +1,156 @@
+// Start Date: 00:00:00 1 January 2000
+pub struct MTime {
+    pub time: u64,
+}
+
+
+pub struct Date {
+    pub year: String,
+    pub month: String,
+    pub day: String,
+    pub hour: String,
+    pub minute: String,
+    pub second: String,
+}
+
+impl Date {
+    pub fn format(&self, to_format: &str) -> String {
+        let formatted_string = to_format
+            .replace("%s", &self.second.to_string())
+            .replace("%m", &self.minute.to_string())
+            .replace("%h", &self.hour.to_string())
+            .replace("%D", &self.day.to_string())
+            .replace("%M", &self.month.to_string())
+            .replace("%Y", &self.year.to_string());
+        formatted_string
+    }
+}
+
+impl MTime {
+    pub fn new(time: u64) -> MTime {
+        MTime {
+            time
+        }
+    }
+
+    pub fn get_count_days(&self) -> u64 {
+        self.time / 86400
+    }
+    
+    pub fn get_count_years(&self) -> u64 {
+        self.get_count_days() / 365
+    }
+
+    pub fn is_leap_year(&self) -> bool {
+        self.get_count_days() % (365 * 3 + 366) <= 366
+    }
+    
+    pub fn get_seconds(&self) -> u8 {
+        let seconds = self.time % 60; 
+        seconds as u8
+    }
+
+    pub fn get_visual_seconds(&self) -> String {
+        if self.get_seconds() < 10 {"0".to_string() + self.get_seconds().to_string().as_str()} else {self.get_seconds().to_string()} 
+    }
+ 
+    pub fn get_minutes(&self) -> u8 {
+        let minutes = (self.time / 60 ) % 60; 
+        minutes as u8
+    }
+ 
+    pub fn get_visual_minutes(&self) -> String {
+        if self.get_minutes() < 10 {"0".to_string() + self.get_minutes().to_string().as_str()} else {self.get_minutes().to_string()} 
+    }
+    
+    pub fn get_hours(&self) -> u8 {
+        let hours = (self.time / 3600) % 24; 
+        hours as u8
+    }
+
+    pub fn get_visual_hours(&self) -> String {
+        if self.get_hours() < 10 {"0".to_string() + self.get_hours().to_string().as_str()} else {self.get_hours().to_string()} 
+    }
+
+    pub fn get_day(&self) -> u8 {
+        let mut days = self.get_count_days();
+        let mut month_array: [u64; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let mut i: usize = 0;
+        while month_array[i]<days {
+            if self.is_leap_year() && i == 0 {
+                month_array[1] += 1;
+            }
+            days -= month_array[i];
+            i += 1;
+            if i == 12 {
+                i = 0;
+            }
+        }
+        if days==0 {(days+1) as u8} else {days as u8}
+    }
+    
+    pub fn get_visual_day(&self) -> String {
+        if self.get_day() < 10 {"0".to_string() + self.get_day().to_string().as_str()} else {self.get_day().to_string()} 
+    }
+
+    pub fn get_month(&self) -> u8 {
+        let mut days = self.get_count_days();
+        let mut month_array: [u64; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let mut i: usize = 0;
+        while month_array[i]< days {
+            if self.is_leap_year() && i == 0 {
+                month_array[1] += 1;
+            }
+            days -= month_array[i];
+            i += 1;
+            if i == 12 {
+                i = 0;
+            }
+        }
+        (i+1) as u8
+    }
+
+    pub fn get_visual_month(&self) -> String {
+        if self.get_month() < 10 {"0".to_string() + self.get_month().to_string().as_str()} else {self.get_month().to_string()} 
+    }
+
+    pub fn get_month_name(&self) -> &str {
+        const MONTHS: [&str; 12] = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        let mut days = self.get_count_days();
+        let mut month_array: [u64; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let mut i: usize = 0;
+        while month_array[i]< days {
+            if self.is_leap_year() && i == 0 {
+                month_array[1] += 1;
+            }
+            days -= month_array[i];
+            i += 1;
+            if i == 12 {
+                i = 0;
+            }
+        }
+        MONTHS[i]
+    }
+
+    pub fn get_year(&self) -> u64 {
+        2000 + self.get_count_years()
+    }
+
+    pub fn get_visual_year(&self) -> String {
+        (2000 + self.get_count_years()).to_string()
+    }
+
+    pub fn get_date(&self) -> Date {
+        Date {
+            year: self.get_visual_year(),
+            month: self.get_visual_month(),
+            day: self.get_visual_day(),
+            hour: self.get_visual_hours(),
+            minute: self.get_visual_minutes(),
+            second: self.get_visual_seconds(),
+        }
+    }
+}
